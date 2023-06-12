88f40dbeda77e3f9a20099b10ad25d0bcb583102
eltonrp <eltonrpo@gmail.com>
2023-06-12 13:40:01 -0300

    eslint and prettier config

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,14 +1,13 @@
-import { StatusBar } from 'expo-status-bar';
-import { Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar'
+import { Text, View } from 'react-native'
 
-const string = 'Hello World!'
+const string: String = 'Hello World!'
 
 export default function App() {
   return (
-    <View className='bg-black flex-1 justify-center items-center'>
-      <Text className='font-bold text-white text-3xl'>{string}</Text>
+    <View className="flex-1 items-center justify-center bg-black">
+      <Text className="text-3xl font-bold text-white">{string}</Text>
       <StatusBar style="auto" />
     </View>
-  );
+  )
 }
-
