eb6f47401939cc50a883053daeda326b89508ae6
eltonrp <eltonrpo@gmail.com>
2023-07-07 00:33:44 -0300

    fav, fonts, colors config

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,7 +1,11 @@
 export default function Home() {
   return (
-    <div className="flex h-screen items-center justify-center bg-slate-50 text-center text-4xl font-bold">
-      Teste
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left Column */}
+      <div></div>
+
+      {/* Right Column */}
+      <div></div>
+    </main>
   )
 }
